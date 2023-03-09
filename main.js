ace7718dce0ab5c6a229b94723ed3cb9ba3ce293
ZPitta <pittadeoliveiraj@gmail.com>
2023-03-09 14:59:00 -0300

    Projeto Finalizado com o suporte do curso Start Na Programação da OneBitCode

--- main.js
+++ main.js
@@ -1,21 +1,38 @@
-function calcular(valor1, operacao, valor2) {
-    if(valor1 == isNaN || valor2 == isNaN) {
-        let txt = document.innerHTML("<br /><p>Impossivel calcular</p>");
-        return txt;
-    }else if(operacao == "NA"){
-        let txt = document.innerHTML("<br /><p>Você deve selecionar um valor para operação</p>");
-        return txt;
-    }else if(operacao == "+"){
-        let result = document.body.innerHTML("<br /><p>O resultado é: </p>" + (valor1 + valor2));
-        return result;
-    }else if(operacao == "-"){
-        let result = document.body.innerHTML("<br /><p>O resultado é: </p>" + (valor1 - valor2));
-        return result;
-    }else if(operacao == "*"){
-        let result = document.body.innerHTML("<br /><p>O resultado é: </p>" + (valor1 * valor2));
-        return result;
-    }else if(operacao == "/"){
-        let result = document.body.innerHTML("<br /><p>O resultado é: </p>" + (valor1 / valor2));
-        return result;
+function calcular(event){
+    event.preventDefault();
+
+    let n1 = document.getElementById('num1').value;
+    let n2 = document.getElementById('num2').value;
+    let op = document.getElementById('operacao').value;
+
+    n1 = parseFloat(n1);
+    n2 = parseFloat(n2);
+
+    if(op == '' || op == 'NA'){
+        alert("Escolha uma operação");
+        return;
     }
+
+   if(op == '+'){
+        resultado = n1 + n2;
+    }
+    if(op == '-'){
+        resultado = n1 - n2;
+    }
+    if(op == '*'){
+        resultado = n1 * n2;
+    }
+    if(op == '/'){
+        resultado = n1 / n2;
+    }
+
+    document.getElementById('resultado').style.display = 'block';
+    document.getElementById('result').style.display = 'block';
+
+    document.getElementById('result').innerHTML = resultado;
 }
+
+document.getElementById('resultado').style.display = 'none'
+document.getElementById('result').style.display = 'none'
+
+document.getElementById('formCalc').addEventListener('submit', calcular)
